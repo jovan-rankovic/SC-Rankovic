15f4b2d902ace676f180e4f4683a1349ece2b234
Jovan Ranković <jovan-rankovic@users.noreply.github.com>
2019-09-17 08:41:35 +0200

    prevented submit on keyup event

--- resources/js/components/energijapp/operator/users/CardNumberSearch.vue
+++ resources/js/components/energijapp/operator/users/CardNumberSearch.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form @submit.prevent>
         <div class="form-group">
             <div class="form-line">
                 <input ref="cn" name="card_number" id="card_number" type="text" maxlength="5" class="form-control text-center" placeholder="Broj kartice (skener)" v-model="card_number" @keyup="cardNumberSearch" autofocus />
@@ -22,13 +22,13 @@
                         card_number: this.card_number
                     }
                 })
-                .then(response => {
-                    if(response.data[0].id != null)
-                        location.replace('/energijapp/profile/' + response.data[0].id)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
+                    .then(response => {
+                        if(response.data[0].id != null)
+                            location.replace('/energijapp/profile/' + response.data[0].id)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
             }
         }
     }
